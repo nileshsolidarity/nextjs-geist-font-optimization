54ff05d5fb154c5aba1863cecd99e42f20e4168c
nileshsolidarity <142440683+nileshsolidarity@users.noreply.github.com>
2025-07-13 19:04:02 +0530

    Add .next/static/chunks/src_app_reports_page_tsx_626de5f4._.js 1752413641787

---
+++ .next/static/chunks/src_app_reports_page_tsx_626de5f4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7e8aa2c7._.js",
+  "static/chunks/node_modules_2d615e05._.js"
+],
+    source: "dynamic"
+});
